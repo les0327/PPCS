72254349270451176a6536d55c91e83a9b94d946
les0327 <les.volodumur@gmail.com>
2018-03-28 18:42:37 +0300

    Start Lab3

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            int size = 4;
+            Threads threads = new Threads(size);
+
+            Thread t1 = new Thread(threads.Task1);
+            Thread t2 = new Thread(threads.Task2);
+            Thread t3 = new Thread(threads.Task3);
+            Thread t4= new Thread(threads.Task4);
+            Thread t5= new Thread(threads.Task5);
+            Thread t6= new Thread(threads.Task6);
+
+            t1.Name = "Task1";
+            t2.Name = "Task2";
+            t3.Name = "Task3";
+
+            t1.Priority = ThreadPriority.Highest;
+            t2.Priority = ThreadPriority.Normal;
+            t3.Priority = ThreadPriority.Lowest;
+
+            Console.WriteLine("Main start");
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+            Console.WriteLine("Main finish");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab3/Lab3/Threads.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab3
+{
+    public class Threads
+    {
+        private readonly int _size;
+
+        public Threads(int size)
+        {
+            _size = size;
+        }
+
+        public void Task1()
+        {
+
+        }
+
+        public void Task2()
+        {
+            
+        }
+
+        public void Task3()
+        {
+            
+        }
+
+        public void Task4()
+        {
+
+        }
+
+        public void Task5()
+        {
+
+        }
+
+        public void Task6()
+        {
+
+        }
+    }
+}
